eb886c2bc9ae4efe00ed554be96faee7a1087e35
Juliet Oberding <juliet.oberding@gmail.com>
2019-08-23 00:13:32 -0700

    Add username from user data

--- src/Profile.js
+++ src/Profile.js
@@ -4,14 +4,14 @@ import { useBlockstack} from 'react-blockstack'
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
 export default function Profile (props) {
-  const { signOut, person } = useBlockstack()
+  const { signOut, person, userData } = useBlockstack()
   return (
     person ?
     <div className="panel-welcome" id="section-2">
       <div className="avatar-section">
         <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
       </div>
-      <h1>Hello, <span id="heading-name">{ person.username() ? person.username() : 'Nameless Person' }</span>!</h1>
+      <h1>Hello, <span id="heading-name"> { person.name() ? person.name() : 'Nameless Person' } aka {userData.username}</span>!</h1>
       <p className="lead">
         <button
           className="btn btn-primary btn-lg"
