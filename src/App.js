7ff149016c4a6fa0043134812566bdf671325180
Juliet Oberding <juliet.oberding@gmail.com>
2019-09-02 00:19:19 -0700

    Added personal menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Profile from './Profile.js';
+import Sidebar,{Menu, MenuItem} from './library/Sidebar.jsx';
 
 export default function App (props) {
   return (
-    <div className="site-wrapper">
-      <div className="site-wrapper-inner">
+    <div className="row">
+      <div className="col-auto">
+         <Sidebar>
+          <Menu>
+            <MenuItem title = "favorites" icon = "fas fa-heart"/>
+            <MenuItem title = "my apps" icon = "fas fa-bookmark" />
+          </Menu>
+
+         </Sidebar>
+      </div>
+      <div className="col">
          <Profile />
       </div>
     </div>
