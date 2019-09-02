f191d9581a44d8f63be234547f69ca49cbb15b92
Juliet Oberding <juliet.oberding@gmail.com>
2019-09-02 00:35:05 -0700

    Add menu items

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ export default function App (props) {
       <div className="col-auto">
          <Sidebar>
           <Menu>
-            <MenuItem title = "favorites" icon = "fas fa-heart"/>
-            <MenuItem title = "my apps" icon = "fas fa-bookmark" />
+            <MenuItem id="favorites" title = "Favorites" icon = "fas fa-heart"/>
+            <MenuItem id="used" title = "My Apps" icon = "fas fa-bookmark" />
           </Menu>
-
+          <Menu>
+            <MenuItem id="productivity" title = "Productivity" icon = "fas fa-folder"/>
+            <MenuItem id="digital rights"  title = "Digital Rights" icon = "fas fa-folder" />
+          </Menu>
+          <Menu>
+            <MenuItem id="top" title = "Top Ranking" icon = "fas fa-certificate"/>
+            <MenuItem id="recommended" title = "Recommended" icon = "fas fa-thumbs-up" />
+            <MenuItem id="new" title = "New Releases" icon = "fas fa-lightbulb" />
+          </Menu>
          </Sidebar>
       </div>
       <div className="col">
