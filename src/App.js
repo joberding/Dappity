18c75050b936b11c8cf9ffd41700e7945e5493f6
Juliet Oberding <juliet.oberding@gmail.com>
2019-09-01 23:34:30 -0700

    Reorganize signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Profile from './Profile.js';
-import Signin from './Signin.js';
 
 export default function App (props) {
   return (
     <div className="site-wrapper">
       <div className="site-wrapper-inner">
          <Profile />
-         <Signin  />
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.js'
 import Auth from './Auth.js'
+import Signin from './Signin.js'
 import { AppConfig } from 'blockstack';
 import { Blockstack, initBlockstack } from 'react-blockstack'
 
@@ -15,3 +16,4 @@ initBlockstack(appConfig)
 
 ReactDOM.render(<Router><Blockstack><App /></Blockstack></Router>, document.getElementById('root'));
 ReactDOM.render(<Blockstack><Auth /></Blockstack>, document.getElementById('auth'));
+ReactDOM.render(<Blockstack><Signin /></Blockstack>, document.getElementById('signin'));
